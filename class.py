7b1beafedcaf81d28cae907ea0bd9dda0e9735da
monineun <moni28198@gmail.com>
2021-04-07 16:39:19 +0200

    Clasificadores terminados

--- class.py
+++ class.py
@@ -3,131 +3,145 @@ import pandas            as pd
 import numpy             as np
 
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network  import MLPClassifier
-from sklearn.naive_bayes     import GaussianNB
+from sklearn.naive_bayes     import BernoulliNB
+from sklearn.neighbors       import KNeighborsClassifier
 from sklearn.ensemble        import RandomForestClassifier
 from sklearn.dummy           import DummyClassifier
-from sklearn                 import neighbors
+from sklearn.tree            import DecisionTreeClassifier
 
 from scipy.stats             import f_oneway
 from scipy                   import stats
 
 
-# Clasificadores!!!!
 
+# Leemos la matriz de caracteristicas
 data = pd.read_csv('dataset.csv')
 
+# Separamos la variable clase
 X = data.drop(['Rating'], axis=1)
 y = data['Rating']
 
 
 scores = []
-
-# Naive Bayes 
-print('\n ' + '-'*50 + '\n · Clasificador Naive Bayes\n')
-
-clf = GaussianNB()
-aux_scores = []
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='accuracy', cv=10))
-print('    > Accuracy: ' + str(aux_scores[-1]))
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='precision', cv=10))
-print('    > Precision: ' + str(aux_scores[-1]))
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='recall', cv=10))
-print('    > Recall: ' + str(aux_scores[-1]))
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='f1', cv=10))
-print('    > F1 score: ' + str(aux_scores[-1]))
-
-scores.append(aux_scores)
-
-# kNN
-print('\n ' + '-'*50 + '\n · Clasificador kNN\n')
-
-clf = neighbors.KNeighborsClassifier(3)
-aux_scores = []
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='accuracy', cv=10))
-print('    > Accuracy: ' + str(aux_scores[-1]))
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='precision', cv=10))
-print('    > Precision: ' + str(aux_scores[-1]))
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='recall', cv=10))
-print('    > Recall: ' + str(aux_scores[-1]))
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='f1', cv=10))
-print('    > F1 score: ' + str(aux_scores[-1]))
-
-scores.append(aux_scores)
-
-'''
-# Random Forest
-print('\n ' + '-'*50 + '\n · Clasificador Random Forest\n')
-
-clf = RandomForestClassifier()
-aux_scores = []
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='accuracy', cv=10))
-print('    > Accuracy: ' + str(aux_scores[-1]))
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='precision', cv=10))
-print('    > Precision: ' + str(aux_scores[-1]))
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='recall', cv=10))
-print('    > Recall: ' + str(aux_scores[-1]))
-
-aux_scores.append(cross_val_score(clf, X, y, scoring='f1', cv=10))
-print('    > F1 score: ' + str(aux_scores[-1]))
-
-scores.append(aux_scores)
-
-
-
-# Algoritmo basado en reglas?
-print('\n ' + '-'*50 + '\n · DummyClassifier\n')
-
-clf = DummyClassifier()
-
-
-scores = cross_val_score(clf, X, y, scoring='accuracy', cv=10)
-print('    > Accuracy: ' + str(scores))
-
-scores = cross_val_score(clf, X, y, scoring='precision', cv=10)
-print('    > Precision: ' + str(scores))
-
-scores = cross_val_score(clf, X, y, scoring='recall', cv=10)
-print('    > Recall: ' + str(scores))
-
-scores = cross_val_score(clf, X, y, scoring='f1', cv=10)
-print('    > F1 score: ' + str(scores))
-
-
-
-# Red neuronal
-print('\n ' + '-'*50 + '\n · MLPClassifier\n')
-
-clf = MLPClassifier()
-
-
-scores = cross_val_score(clf, X, y, scoring='accuracy', cv=10)
-print('    > Accuracy: ' + str(scores))
-
-scores = cross_val_score(clf, X, y, scoring='precision', cv=10)
-print('    > Precision: ' + str(scores))
-
-scores = cross_val_score(clf, X, y, scoring='recall', cv=10)
-print('    > Recall: ' + str(scores))
-
-scores = cross_val_score(clf, X, y, scoring='f1', cv=10)
-print('    > F1 score: ' + str(scores))
-
+measures = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc']
 
 #print(sorted(sklearn.metrics.SCORERS.keys()))
-'''
 
+
+# CLASIFICADOR NAIVE BAYES
+print('\n ' + '-'*50 + '\n · Clasificador Naive Bayes\n')
+
+# Tuning de parametros
+# params_nv = {'alpha': [0.01, 0.1, 0.5, 1.0, 10.0]}
+# grid_nv = GridSearchCV(BernoulliNB(), params_nv, cv=10, scoring='accuracy')
+# grid_nv.fit(X,y)
+# print(grid_nv.best_params_)
+
+clf = BernoulliNB(alpha=0.01)
+aux_scores = []
+
+for i in measures:
+	aux_scores.append(cross_val_score(clf, X, y, scoring=i, cv=10))
+	print('    > ' + i + ': ' + str(aux_scores[-1]) + '      (MEAN: ' + str(round(aux_scores[-1].mean(), 2)) + ')')
+
+scores.append(aux_scores)
+
+
+
+# CLASIFICADOR kNN
+print('\n ' + '-'*50 + '\n · Clasificador kNN\n')
+
+# Tuning de parametros
+# params_knn = {'n_neighbors':[1,5,7,10], 'metric':['minkowski', 'jaccard', 'matching']}
+# grid_knn = GridSearchCV(KNeighborsClassifier(), params_knn, cv=10, scoring='accuracy')
+# grid_knn.fit(X,y)
+# print(grid_knn.best_params_)
+
+
+clf = KNeighborsClassifier(n_neighbors=1, metric='minkowski')
+aux_scores = []
+
+for i in measures:
+	aux_scores.append(cross_val_score(clf, X, y, scoring=i, cv=10))
+	print('    > ' + i + ': ' + str(aux_scores[-1]) + '      (MEAN: ' + str(round(aux_scores[-1].mean(), 2)) + ')')
+
+scores.append(aux_scores)
+
+
+
+# CLASIFICADOR DECISION TREE
+print('\n ' + '-'*50 + '\n · Decision Tree\n')
+
+# Tuning de parametros
+# params__dt = {'criterion':['gini','entropy'],'max_depth':[5,10,30,70,120]}
+# grid__dt = GridSearchCV(RandomForestClassifier(), params__dt,cv=10, scoring='accuracy')
+# grid__dt.fit(X, y)
+# print(grid__dt.best_params_)
+
+
+clf = DecisionTreeClassifier(criterion='entropy', max_depth=30)
+
+aux_scores = []
+
+for i in measures:
+	aux_scores.append(cross_val_score(clf, X, y, scoring=i, cv=10))
+	print('    > ' + i + ': ' + str(aux_scores[-1]) + '      (MEAN: ' + str(round(aux_scores[-1].mean(), 2)) + ')')
+
+scores.append(aux_scores)
+
+
+
+# CLASIFICADOR RADOM FOREST
+print('\n ' + '-'*50 + '\n · Clasificador Random Forest\n')
+
+# Tuning de parametros
+# params_rf = {'n_estimators': [550,600,650], 'max_depth': [14,16,18]}
+# grid_rf = GridSearchCV(RandomForestClassifier(), params_rf,cv=10, scoring='accuracy')
+# grid_rf.fit(X, y)
+# print(grid_rf.best_params_)
+
+clf = RandomForestClassifier(max_depth=18, n_estimators=550)
+aux_scores = []
+
+for i in measures:
+	aux_scores.append(cross_val_score(clf, X, y, scoring=i, cv=10))
+	print('    > ' + i + ': ' + str(aux_scores[-1]) + '      (MEAN: ' + str(round(aux_scores[-1].mean(), 2)) + ')')
+
+scores.append(aux_scores)
+
+
+
+# CLASIFICADOR RED NEURONAL
+print('\n ' + '-'*50 + '\n · MLPClassifier\n')
+
+# Tuning de parametros
+# params_mlp = {'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)],
+# 		      'activation': ['tanh', 'relu'],
+# 		      'solver': ['sgd', 'adam'],
+# 		      'alpha': [0.0001, 0.05],
+# 		      'learning_rate': ['constant','adaptive']}
+# grid_mlp = GridSearchCV(MLPClassifier(), params_mlp,cv=10, scoring='accuracy')
+# grid_mlp.fit(X, y)
+# print(grid_mlp.best_params_)
+
+
+clf = MLPClassifier(activation='tanh', alpha=0.0001, 
+	                hidden_layer_sizes=(50, 100, 50), 
+	                learning_rate='adaptive', solver='adam')
+aux_scores = []
+
+for i in measures:
+	aux_scores.append(cross_val_score(clf, X, y, scoring=i, cv=10))
+	print('    > ' + i + ': ' + str(aux_scores[-1]) + '      (MEAN: ' + str(round(aux_scores[-1].mean(), 2)) + ')')
+
+scores.append(aux_scores)
+
+
+
+
+'''
 # Shapiro Wilk
 print('\n\n' + '~'*50 + '\n · S H A P I R O   W I L K\n' + '~'*50)
 
@@ -173,3 +187,4 @@ for j in range(len(scores[0])):
 		print('     (No hay diferencia...)\n')
 	else:
 		print('     (Hay diferencia!!!)\n')
+'''
--- sna.py
+++ sna.py
@@ -8,31 +8,22 @@ from   collections            import Counter
 from   igraph                 import Graph
 
 
-def info(data):
-	print('\n' + '·'*50 + '\nData head:')
-	print(data)
-	print('Dimensionalidad de la tabla: ' + str(data.shape[0]) + 'x' + str(data.shape[1]))
-
-
 #Caragamos los datos
 data = pd.read_csv('user_rating.txt', sep='\t', header=None, names=['User A', 'User B', 'Rating', 'Date'])
 data = data.drop(['Date'], axis=1)
-info(data)
+print('\n\n · D A T A S E T   O R I G I N A L\n' + '·'*45)
+print(data)
 
 
 #Reducimos el numero de nodos
-num_nodes = 500
+num_nodes = 1000
 node_list = sorted(data['User A'].unique())[:num_nodes]
-node_list_rep = range(len(node_list))
-print('\nNos quedamos con ' + str(len(node_list)) + ' nodos')
 
 data = data[data['User A'].isin(node_list)]
 data = data[data['User B'].isin(node_list)]
 
 data = data.reset_index()
 data = data.drop(['index'], axis=1)
-
-#info(data)
 
 
 #Reseteamos los IDs de los nodos
@@ -44,11 +35,11 @@ un_T = sorted(un_A)
 
 
 for i in range(len(un_T)):
-	# print(' Remplazamos ' + str(un_T[i]) + ' por ' + str(i))
 	data['User A'] = data['User A'].replace([un_T[i]],i)
 	data['User B'] = data['User B'].replace([un_T[i]],i)
 
-info(data)
+print('\n\n · D A T A S E T   P R E P R O C E S A D O\n' + '·'*45)
+print(data)
 
 un_A = sorted(data['User A'].unique())
 un_B = sorted(data['User B'].unique())
@@ -57,137 +48,97 @@ un_A.extend([element for element in un_B if element not in un_A])
 un_T = sorted(un_A)
 
 num_nodes = len(un_T)
-print('\nNúmero de nodos ' + str(num_nodes))
+print('\n Número de nodos: ' + str(num_nodes))
 
 
-#Matriz de adyacencia
-print('\n' + '·'*50 + '\nCreamos la matriz de adyacencia...')
+
+#Creamos la matriz de adyacencia
 adj_mat = np.zeros((num_nodes, num_nodes))
 
 for indice_fila, fila in data.iterrows():
 	adj_mat[fila['User A']][fila['User B']] = fila['Rating']
 
-#print(adj_mat)
-
-feat_mat = data.copy()
-print('\n' + '·'*50 + '\nMatriz de caracteristicas')
-print(feat_mat)
 
 
-print('\n' + '·'*50 + '\nCreamos el grafo con Igraph...')
+#Creamos el grafo con igraph a partir de la matriz de adyacencia
 G = Graph.Weighted_Adjacency(adj_mat)
-#print(G.summary(verbosity=1))
+
+
+# Creamos la matriz de características
+feat_mat = data.copy()
 
 
 # MEDIDAS A NIVEL DE GRAFO
-# Av path lenght
 C1 = []
 
-# assortativity_degree
 C2 = []
 
-# transitivity_undirected
 C3 = []
 
-# transitivity_avglocal_undirected
 C4 = []
 
-# reciprocity
-C5 = []
 
-
+# Recorremos cada arista del grafo
 for indice_fila, fila in data.iterrows():
 	a = int(fila['User A'])
 	b = int(fila['User B'])
 
-	print('\n Boramos ' + str(a) + '->' + str(b))
+	# Borramos la arista
 	G.delete_edges([(a,b)])
 
 	# Caracteristicas
 	# ~~~~~~~~~~~~~~~
 
-	# Av path lenght
 	x = G.average_path_length()
 	C1.append(x)
-	print(' > average_path_length: ' + str(x))
 
-	# assortativity_degree
 	x = G.assortativity_degree()
 	C2.append(x)
-	print(' > assortativity_degree: ' + str(x))
 
-	# transitivity_undirected
 	x = G.transitivity_undirected()
 	C3.append(x)
-	print(' > transitivity_undirected: ' + str(x))
 
-	# transitivity_avglocal_undirected
-	x = G.transitivity_avglocal_undirected()
-	C4.append(x)
-	print(' > transitivity_avglocal_undirected: ' + str(x))
-
-	# reciprocity
 	x = G.reciprocity()
-	C5.append(x)
-	print(' > reciprocity: ' + str(x))
+	C4.append(x)
 
-	print('\n Anyadimos ' + str(a) + '->' + str(b))
+	# Reincorporamos la arista
 	G.add_edges([(a,b)])
 
-	print('¨' * 60)
 
+# Anyadimos a la matriz de caracteristicas
+feat_mat['AvgPathLen'] = C1
+feat_mat['Assort']     = C2
+feat_mat['Transit']    = C3
+feat_mat['Recipr']     = C4 
 
-feat_mat['G.Av path']    = C1
-feat_mat['G.Assort d'] = C2
-feat_mat['G.Trans']      = C3
-feat_mat['G.Trans Avg']   = C4
-feat_mat['G.Reciprocity'] = C5 
-
-
-'''
-# MEDIDAS A NIVEL DE ARISTA
-#Esto probablemente es hacer trampa!!
-feat_mat['E.Mutual'] = G.is_mutual()
-feat_mat['E.Between'] = G.edge_betweenness()
-
-print(feat_mat)
-'''
 
 
 # MEDIDAS A NIVEL DE NODO
-# strength
+# TODO: Fix this!
 C1A = []
 C1B = []
 
-# pagerank
 C2A = []
 C2B = []
 
-# harmonic_centrality
 C3A = []
 C3B = []
 
-# eigenvector_centrality
 C4A = []
 C4B = []
 
-# eccentricity
 C5A = []
 C5B = []
 
-# coreness
 C6A = []
 C6B = []
 
-# betweeness
 C7A = []
 C7B = []
 
-# hub_score
 C8A = []
 C8B = []
 
-# authority_score
 C9A = []
 C9B = []
 
@@ -197,106 +148,52 @@ for indice_fila, fila in data.iterrows():
 	a = int(fila['User A'])
 	b = int(fila['User B'])
 
-	print('\n Boramos ' + str(a) + '->' + str(b))
+	# Borramos la arista
 	G.delete_edges([(a,b)])
 
 	# Caracteristicas
 	# ~~~~~~~~~~~~~~~
-
-	# strength
-	print(' > strength')
 	x = G.strength()
-
-	print('  A: ' + str(x[a]))
-	print('  B: ' + str(x[b]))
 
 	C1A.append(x[a])
 	C1B.append(x[b])
 
-
-	# pagerank
-	print(' > pagerank')
 	x = G.pagerank()
-
-	print('  A: ' + str(x[a]))
-	print('  B: ' + str(x[b]))
 
 	C2A.append(x[a])
 	C2B.append(x[b])
 
-
-	# harmonic_centrality
-	print(' > harmonic_centrality')
 	x = G.harmonic_centrality()
-
-	print('  A: ' + str(x[a]))
-	print('  B: ' + str(x[b]))
 
 	C3A.append(x[a])
 	C3B.append(x[b])
 	
-
-	# eigenvector_centrality
-	print(' > eigenvector_centrality')
 	x = G.eigenvector_centrality()
-
-	print('  A: ' + str(x[a]))
-	print('  B: ' + str(x[b]))
 
 	C4A.append(x[a])
 	C4B.append(x[b])
 
-
-	# eccentricity
-	print(' > eccentricity')
 	x = G.eccentricity()
-
-	print('  A: ' + str(x[a]))
-	print('  B: ' + str(x[b]))
 
 	C5A.append(x[a])
 	C5B.append(x[b])	
 
-
-	# coreness
-	print(' > coreness')
 	x = G.coreness()
-
-	print('  A: ' + str(x[a]))
-	print('  B: ' + str(x[b]))
 
 	C6A.append(x[a])
 	C6B.append(x[b])
 
-
-	# betweenness
-	print(' > betweenness')
 	x = G.betweenness()
-
-	print('  A: ' + str(x[a]))
-	print('  B: ' + str(x[b]))
 
 	C7A.append(x[a])
 	C7B.append(x[b])
 
-
-	# hub_score
-	print(' > hub_score')
 	x = G.hub_score()
-
-	print('  A: ' + str(x[a]))
-	print('  B: ' + str(x[b]))
 
 	C8A.append(x[a])
 	C8B.append(x[b])
 
-
-	# authority_score
-	print(' > authority_score')
 	x = G.authority_score()
-
-	print('  A: ' + str(x[a]))
-	print('  B: ' + str(x[b]))
 
 	C9A.append(x[a])
 	C9B.append(x[b])
@@ -304,50 +201,43 @@ for indice_fila, fila in data.iterrows():
 
 
 
-	print('\n Anyadimos ' + str(a) + '->' + str(b))
+	# Reincorporamos la arista
 	G.add_edges([(a,b)])
 
-	print('¨' * 60)
+
+# Anyadimos a la matriz de caracteristicas
+feat_mat['StrengthA'] = C1A
+feat_mat['StrengthB'] = C1B
+
+feat_mat['PagerankA'] = C2A
+feat_mat['PagerankB'] = C2B
+
+feat_mat['HarmCenA'] = C3A
+feat_mat['HarmCenB'] = C3B
+
+feat_mat['EigenCenA'] = C4A
+feat_mat['EigenCenB'] = C4B
+
+feat_mat['EccentrA'] = C5A
+feat_mat['EccentrB'] = C5B
+
+feat_mat['CorenessA'] = C6A
+feat_mat['CorenessB'] = C6B
+
+feat_mat['BetweennA'] = C7A
+feat_mat['BetweennB'] = C7B
+
+feat_mat['HubScrA'] = C8A
+feat_mat['HubScrB'] = C8B
+
+feat_mat['AuthScrA'] = C9A
+feat_mat['AuthScrB'] = C9B
 
 
-feat_mat['N.StrngA'] = C1A
-feat_mat['N.StrngB'] = C1B
 
-feat_mat['N.PagerankA'] = C2A
-feat_mat['N.PagerankB'] = C2B
-
-feat_mat['N.HarmCenA'] = C3A
-feat_mat['N.HarmCenB'] = C3B
-
-feat_mat['N.EigenCenA'] = C4A
-feat_mat['N.EigenCenB'] = C4B
-
-feat_mat['N.EccentrA'] = C5A
-feat_mat['N.EccentrB'] = C5B
-
-feat_mat['N.CorenessA'] = C6A
-feat_mat['N.CorenessB'] = C6B
-
-feat_mat['N.BetweennA'] = C7A
-feat_mat['N.BetweennB'] = C7B
-
-feat_mat['N.HubScoreA'] = C8A
-feat_mat['N.HubScoreB'] = C8B
-
-feat_mat['N.AuthScoreA'] = C9A
-feat_mat['N.AuthScoreB'] = C9B
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~44
-
+# Eliminamos la informacion de los nodos
 feat_mat = feat_mat.drop(['User A'], axis=1)
 feat_mat = feat_mat.drop(['User B'], axis=1)
-
-print('\n' + '·'*50 + '\nM E D I D A S   D E   C E N T R A L I D A D\n' + '·'*50)
-print(feat_mat)
-
-# Observamos la distribucion de clases
-#print(feat_mat['Rating'].value_counts())
 
 
 # Separamos varibles predictoras de variable a predecir para todo el conjunto de datos
@@ -370,26 +260,10 @@ print(counter)
 
 newX['Rating'] = newy
 
-print('\n' + '·'*50 + '\nM E D I D A S   D E   C E N T R A L I D A D\n' + '·'*50)
+print('\n' + '·'*45 + '\n· M E D I D A S   D E   C E N T R A L I D A D ·\n' + '·'*45)
 print(newX)
 
 print(newX['Rating'].value_counts())
 
 
 newX.to_csv('dataset.csv', header=True, index=False)
-
-'''
-# Clasificadores!!!!
-
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-
-X_train, X_test, y_train, y_test = train_test_split(newX, newy, test_size=0.5, random_state=0)
-
-
-# Naive Bayes 
-gnb = GaussianNB()
-y_pred = gnb.fit(X_train, y_train).predict(X_test)
-print("Number of mislabeled points out of a total %d points : %d"% (X_test.shape[0], (y_test != y_pred).sum()))
-'''
